c7788efa4ef0d78b2dceff5df692c79935dcec21
Jeremy Yu <jeremyyu8@gmail.com>
2023-01-20 16:08:42 -0500

    nav bar fixed

--- client/src/components/modules/HomeButton.js
+++ client/src/components/modules/HomeButton.js
@@ -12,7 +12,7 @@ const HomeButton = (props) => {
   return (
     <Link
       to={props.url}
-      className="p-3 px-7 text-white bg-sky-500 text-[14px] rounded-md align-baseline border-0 transition-colors duration-250 hover:bg-sky-300 cursor-pointer no-underline"
+      className="py-3 px-7 text-white bg-sky-500 text-[14px] rounded-md align-baseline border-0 transition-colors duration-250 hover:bg-sky-300 cursor-pointer no-underline"
     >
       {props.text}
     </Link>
--- client/src/components/modules/Navbar.js
+++ client/src/components/modules/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = (props) => {
         <div className="hidden md:flex space-x-6">
           <Link
             to="/login"
-            className="no-underline text-blue-500 text-center text-[15px] py-3 transition-colors duration-250 hover:text-sky-400"
+            className="no-underline text-blue-500 text-center text-[15px] my-auto transition-colors duration-250 hover:text-sky-400"
           >
             Login
           </Link>
@@ -42,16 +42,16 @@ const Navbar = (props) => {
 
   return (
     <>
-      <nav className="fixed h-[10vh] w-screen top-0 bg-white border-0 border-gray-200 border-solid border-b-2 z-10">
-        <div className="flex items-center justify-between p-4 px-8">
+      <nav className="fixed h-[10vh] w-screen top-0 bg-white border-0 border-gray-200 border-solid border-b-2 z-10 flex justify-between">
+        <div className="my-auto px-8">
           <Link
             to="/"
             className="text-[36px] font-[900] my-auto hover:cursor-pointer text-black no-underline"
           >
             MazeEd
           </Link>
-          <div> {rightside}</div>
         </div>
+        <div className="my-auto mr-8"> {rightside}</div>
       </nav>
       <div className="h-[10vh] mb-[8vh]"></div>
     </>
