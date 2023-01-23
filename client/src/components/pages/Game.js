911dadeb018e2ec940c41495ec28938a532145bb
andrew <andrewliu278@gmail.com>
2023-01-23 13:46:17 -0500

    merging changes

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -1,20 +1,39 @@
 import React, { useEffect, useRef, useState } from "react";
-import { socket, move, updateWindowSize } from "../../client-socket.js";
+import { socket, move, updateWindowSize, startGame } from "../../client-socket.js";
 
 import { drawCanvas } from "../../canvasManager";
 import { Redirect } from "@reach/router";
 import { get, post } from "../../utilities.js";
 
 import Question from "../modules/GamePageComponents/Question.js";
+import TeacherGamePage from "../modules/GamePageComponents/TeacherGamePage.js";
 
 const Game = () => {
+  // metadata
   const [gamePin, setGamePin] = useState(undefined); // user game pin
   const [userData, setUserData] = useState(undefined); // user data
+  const [status, setStatus] = useState("lobby");
+
+  // redirect logic
   const [redirect, setRedirect] = useState(false);
-  const [level, setLevel] = useState("lobby");
+
+  // flashcards
   const [questionShowing, setQuestionShowing] = useState(false);
   const [flashCardSet, setFlashCardSet] = useState(undefined);
   const [curQuestion, setCurQuestion] = useState(undefined);
+  const [newLevelCard, setNewLevelCard] = useState(false);
+
+  // game data
+  const [level, setLevel] = useState(undefined);
+  const [tokens, setTokens] = useState(undefined);
+  const [speed, setSpeed] = useState(undefined);
+  const [power, setPower] = useState(undefined);
+
+  // dimension of game window
+  const [windowDimension, setWindowDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
   const canvasRef = useRef(null); // canvas reference
 
   // authentication
@@ -35,11 +54,8 @@ const Game = () => {
   // load game pin
   useEffect(() => {
     socket.on("receivePin", (data) => {
-      console.log(data);
       setGamePin(data.pin);
       setFlashCardSet(data.cards);
-      console.log("gamepin", gamePin);
-      console.log("pin", data.pin);
     });
   }, [gamePin, userData]);
 
@@ -47,23 +63,21 @@ const Game = () => {
 
   useEffect(() => {
     socket.on("update", (update) => {
-      // console.log(update);
       if (userData && gamePin) {
         processUpdate(update[gamePin], userData._id);
-      } else {
-        // console.log("no user data?");
       }
     });
-    // drawCanvas({ p: { x: 0, y: 0 } }, canvasRef);
   }, [userData, gamePin]);
 
   // add event listener to user key inputs
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     window.addEventListener("keyup", handleKeyUp);
+    window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
       window.removeEventListener("keyup", handleKeyUp);
+      window.removeEventListener("resize", handleResize);
     };
   }, [userData, gamePin]);
 
@@ -79,9 +93,21 @@ const Game = () => {
     }
   }, [userData, gamePin]);
 
+  // update window size whenever it changes
+  const handleResize = () => {
+    console.log("handle resize");
+    if (userData && gamePin) {
+      updateWindowSize({
+        x: window.innerWidth,
+        y: window.innerHeight,
+        _id: userData._id,
+        pin: gamePin,
+      });
+      setWindowDimension({ width: window.innerWidth, height: window.innerHeight });
+    }
+  };
+
   const handleKeyUp = (e) => {
-    console.log(userData);
-    console.log(gamePin);
     if (e.key === "ArrowUp") {
       pressed["up"] = false;
     }
@@ -119,6 +145,21 @@ const Game = () => {
 
   const processUpdate = (update, _id) => {
     drawCanvas(update, canvasRef, _id);
+    if (update["status"] !== status) {
+      setStatus(update["status"]);
+    }
+    if (update["players"][_id]["level"] !== level) {
+      setLevel(update["players"][_id]["level"]);
+    }
+    if (update["players"][_id]["tokens"] !== tokens) {
+      setTokens(update["players"][_id]["tokens"]);
+    }
+    if (update["players"][_id]["speed"] !== speed) {
+      setSpeed(update["players"][_id]["speed"]);
+    }
+    if (update["players"][_id]["power"] !== power) {
+      setPower(update["players"][_id]["power"]);
+    }
   };
 
   const handleNewQuestion = () => {
@@ -128,16 +169,30 @@ const Game = () => {
     }
   };
 
+  const handleStartGame = () => {
+    console.log("handling start game");
+    setStatus("game");
+    startGame(gamePin);
+  };
+
   return (
     <>
       {redirect ? (
         <Redirect from="/game" to="/login" />
       ) : (
         <>
-          <div className="fixed w-full h-full z-0">
-            <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} />
-          </div>
-          {level === "lobby" && userData && userData.role === "teacher" && gamePin && (
+          {userData &&
+            ((userData.role === "teacher" && status === "lobby") ||
+              userData.role === "student") && (
+              <div className="fixed w-full h-full z-0">
+                <canvas
+                  ref={canvasRef}
+                  width={windowDimension.width}
+                  height={windowDimension.height}
+                />
+              </div>
+            )}
+          {status === "lobby" && userData && userData.role === "teacher" && gamePin && (
             <>
               <div className="bg-white bg-opacity-30 fixed z-10 w-full h-auto p-[4vh]">
                 <div className="text-center text-4xl">Join at localhost:5050</div>
@@ -145,17 +200,56 @@ const Game = () => {
               </div>
               <div className="bg-white bg-opacity-30 fixed z-10 w-[100%] h-auto bottom-0">
                 <div className="flex justify-between text-3xl">
-                  <div className="p-[3vh]">Active players</div>
-                  <div className="p-[3vh]">Start Game</div>
-                  <div className="p-[3vh]">Rules and settings</div>
+                  <div className="p-[3vh] hover:text-blue-500 cursor-pointer transition-all text-md">
+                    Active players
+                  </div>
+                  <div
+                    className="p-[3vh] hover:text-blue-500 cursor-pointer transition-all text-md"
+                    onClick={handleStartGame}
+                  >
+                    Start Game
+                  </div>
+                  <div className="p-[3vh] hover:text-blue-500 cursor-pointer transition-all text-md">
+                    Rules and settings
+                  </div>
                 </div>
               </div>
             </>
           )}
-          {level !== "lobby" && userData && userData.role === "student" && gamePin && (
-            <button onClick={handleNewQuestion} className="">
-              Answer Question
-            </button>
+          {status !== "lobby" && userData && userData.role === "student" && gamePin && (
+            <>
+              <button onClick={handleNewQuestion} className="">
+                Answer Question
+              </button>
+              <div className="bg-white bg-opacity-30 fixed z-10 w-[20vh] h-[20vh] top-[2vh] border-solid">
+                <div className="text-center text-xl mt-[2vh]">Upgrades</div>
+                <div className="my-[1vh] mx-[2vw]">
+                  <table className="table-auto text-md">
+                    <tbody>
+                      <tr>
+                        <td>Tokens:</td> <td>{tokens}</td>
+                      </tr>
+                      <tr>
+                        <td>Speed:</td> <td>{speed}</td>
+                      </tr>
+                      <tr>
+                        <td>Power:</td> <td>{power}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div className="bg-white bg-opacity-30 fixed z-10 w-[25vh] h-[25vh] bottom-[2vh] right-0 border-solid">
+                <div>Minimap</div>
+                <div>Level: {level}</div>
+              </div>
+            </>
+          )}
+          {status !== "lobby" && userData && userData.role === "teacher" && gamePin && (
+            <>
+              <TeacherGamePage />
+              <div>pin: {gamePin}</div>
+            </>
           )}
           {questionShowing && (
             <Question
