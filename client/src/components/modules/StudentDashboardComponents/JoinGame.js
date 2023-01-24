d624c00094b4480201ab3efbee9b37db962478ec
soch11 <sophia.nyc2004@gmail.com>
2023-01-23 23:47:55 -0500

    student join game

--- client/src/components/modules/StudentDashboardComponents/JoinGame.js
+++ client/src/components/modules/StudentDashboardComponents/JoinGame.js
@@ -39,19 +39,21 @@ const JoinGame = (props) => {
         <Redirect from="/student" to="/game" />
       ) : (
         <>
-          <div className="flex flex-col max-w-[50%] mx-auto mt-20 border-solid border-black rounded-xl p-4">
-            <div className="mx-auto my-4 mb-10 text-5xl">Join Game</div>
-            <input
-              className="my-4 border-solid text-2xl"
-              placeholder="Game Code"
-              onInput={handleChange}
-            ></input>
-            <button
-              className="mx-auto my-4 text-5xl border-solid hover:bg-sky-300 cursor-pointer transition-all"
-              onClick={() => handleJoin(props.userId, props.userName)}
-            >
-              Enter
-            </button>
+          <div className="bg-spaceimg2 bg-fixed bg-cover h-screen flex flex-col items-center justify-center">
+            <div className="rounded-3xl bg-zinc-900 bg-opacity-80 px-16 py-10 shadow-lg max-sm:px-8 flex flex-col items-center justify-center">
+              <div className="mx-auto mb-6 text-5xl text-blue-200">Join Game</div>
+              <input
+                className="my-4 text-2xl"
+                placeholder="Game Code"
+                onInput={handleChange}
+              ></input>
+              <button
+                className="mx-auto my-4 text-5xl rounded-xl text-blue-200 bg-blue-800 hover:bg-blue-500 cursor-pointer transition-all"
+                onClick={() => handleJoin(props.userId, props.userName)}
+              >
+                Enter
+              </button>
+            </div>
           </div>
         </>
       )}
