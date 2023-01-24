e3f6f22799f573170752d631891e7e199afb603c
soch11 <sophia.nyc2004@gmail.com>
2023-01-23 22:02:05 -0500

    home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -65,52 +65,41 @@ const Home = ({ userId, userRole, userName }) => {
           <div className="bg-stone-900 bg-fixed bg-cover h-screen flex flex-col justify-evenly">
             <div className="flex flex-wrap justify-evenly">
               <div className="p-4 xl:w-1/3 md:w-1/2 w-7/12 mb-12">
-                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-80 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
-                  <div class="text-4xl text-blue-100 mt-4 ml-4">Flashcards</div>
-                  <div className="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900">
+                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-90 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
+                  <div class="text-4xl text-blue-100 mt-8 ml-8">Flashcards</div>
+                  <div className="pt-4 pl-8 pr-8 flex rounded-lg items-center text-gray-900 text-lg">
+                    <p>Create your own set of flashcards, custom to your classroom.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-4 xl:w-1/3 md:w-1/2 w-7/12 mb-12">
+                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-90 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
+                  <div class="text-4xl text-blue-100 mt-8 ml-8">Create</div>
+                  <div className="pt-4 pl-8 pr-8 flex rounded-lg items-center text-gray-900 text-lg">
+                    <p>Create and join unique game lobbies.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-4 xl:w-1/3 md:w-1/2 w-7/12 mb-12">
+                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-90 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
+                  <div class="text-4xl text-blue-100 mt-8 ml-8">MazED</div>
+                  <div className="pt-4 pl-8 pr-8 flex rounded-lg items-center text-gray-900 text-lg">
                     <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi unde maiores
-                      nihil officiis voluptatum numquam quisquam, veritatis placeat aliquid velit,
-                      saepe, quae totam cupiditate. Iure quas quam dolorem magnam ut.
+                      Answer questions to escape the maze! Don't get tagged by other players, level
+                      up to tag them first.
                     </p>
                   </div>
                 </div>
               </div>
 
               <div className="p-4 xl:w-1/3 md:w-1/2 w-7/12 mb-12">
-                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-80 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
-                  <div class="text-4xl text-blue-100 mt-4 ml-4">Create</div>
-                  <div className="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900">
+                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-90 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
+                  <div class="text-4xl text-blue-100 mt-8 ml-8">Review</div>
+                  <div className="pt-4 pl-8 pr-8 flex rounded-lg items-center text-gray-900 text-lg">
                     <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi unde maiores
-                      nihil officiis voluptatum numquam quisquam, veritatis placeat aliquid velit,
-                      saepe, quae totam cupiditate. Iure quas quam dolorem magnam ut.
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="p-4 xl:w-1/3 md:w-1/2 w-7/12 mb-12">
-                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-80 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
-                  <div class="text-4xl text-blue-100 mt-4 ml-4">MazED</div>
-                  <div className="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900">
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi unde maiores
-                      nihil officiis voluptatum numquam quisquam, veritatis placeat aliquid velit,
-                      saepe, quae totam cupiditate. Iure quas quam dolorem magnam ut.
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="p-4 xl:w-1/3 md:w-1/2 w-7/12 mb-12">
-                <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-80 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
-                  <div class="text-4xl text-blue-100 mt-4 ml-4">Review</div>
-                  <div className="pt-8 pl-4 pr-4 flex rounded-lg items-center text-gray-900">
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi unde maiores
-                      nihil officiis voluptatum numquam quisquam, veritatis placeat aliquid velit,
-                      saepe, quae totam cupiditate. Iure quas quam dolorem magnam ut.
+                      Review past games and missed questions to enhance your learning experience.
                     </p>
                   </div>
                 </div>
