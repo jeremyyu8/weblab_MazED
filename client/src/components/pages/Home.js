bbac277f51b2825319b45f972d96d5cc529470d1
soch11 <sophia.nyc2004@gmail.com>
2023-01-29 23:51:47 -0500

    dasfkuh

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -106,16 +106,16 @@ const Home = ({ userId, userRole, userName }) => {
                 ></Card>
               </div>
               <div className="flex flex-wrap justify-evenly pb-4">
-                <div className="p-4 mt-8 text-[3vw] xl:w-1/2 md:w-1/3 md:text-[1.5vw] w-7/12">
+                <div className="p-4 mt-8 text-[3vw] xl:w-2/3 md:w-1/3 md:text-[1.5vw] w-7/12">
                   <div className="bg-blue-500 bg-opacity-60 hover:bg-opacity-90 hover:scale-110 transition duration-300 ease-in-out h-full rounded-lg mb-6 flex flex-col relative overflow-hidden shadow-xl">
                     <div class="text-[4vw] text-blue-100 mt-8 ml-8 md:text-[3vw]">Why MazED?</div>
                     <div className="pt-4 pl-8 pr-8 flex rounded-lg items-center text-gray-900">
                       <p className="leading-8">
                         Not only is MazED a fun and engaging way for students to learn, it provides
                         tools such as game analyses that help teachers better understand which
-                        questions their students struggle on in order to help them learn. Teachers
-                        can tailor MazED to their specific classroom needs through creating their
-                        custom flashcard sets and choosing between different game modes and
+                        questions their students struggle on in order to identify how to help them.
+                        Teachers can tailor MazED to their specific classroom needs through creating
+                        their custom flashcard sets and choosing between different game modes and
                         customizations.
                       </p>
                     </div>
